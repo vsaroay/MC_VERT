13d81f7cea210f523452f1af55b991ca9cbb4763
Hippomano <RyanJMorris11@gmail.com>
2017-09-27 12:06:28 -0700

    Added Calendar

---
+++ Calendar/Calendar.js
@@ -0,0 +1,31 @@
+
+
+/*
+
+// load cell information on calendar
+function cellInfo(){
+
+}
+
+// Anonymous Function
+var Click Calendar = document.querySelector('button');
+
+myButton.onclick = function() {
+  alert('hello');
+}
+*/
+//________________________________ above is notes _______________________________//
+
+
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August",
+    "September", "October", "November", "December"]
+
+
+var table = document.getElementById('Calendar');
+var cells = table.getElementsByTagName("td");
+// loop loads cell information on calendar
+
+for (var i = 0; i < cells.length; i++) {
+
+
+}
